27518ad0c042422a1c189e93ff64810190d1e9f2
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-02-14 15:57:17 +0300

    NuGet EntityFramework updated

--- DataAccess/Concrete/EntityFramework/EFCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EFCarDal.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
@@ -10,27 +12,50 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Car entity)
         {
-            throw new NotImplementedException();
+            using (var context = new CarRentalContext())
+            {
+                var entityAdded = context.Entry(entity);
+                entityAdded.State = EntityState.Added;
+                context.SaveChanges();
+            }
         }
 
         public void Update(Car entity)
         {
-            throw new NotImplementedException();
+            using (var context = new CarRentalContext())
+            {
+                var entityDeleted = context.Entry(entity);
+                entityDeleted.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public void Delete(Car entity)
         {
-            throw new NotImplementedException();
+            using (var context = new CarRentalContext())
+            {
+                var entityUpdated = context.Entry(entity);
+                entityUpdated.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
 
         public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (var context = new CarRentalContext())
+            {
+                return filter == null
+                    ? context.Set<Car>().ToList()
+                    : context.Set<Car>().Where(filter).ToList();
+            }
         }
 
         public Car Get(Expression<Func<Car, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (var context = new CarRentalContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
         }
     }
 }
