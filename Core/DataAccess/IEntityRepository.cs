9ceb17bb4c650bee7e1641fa1d41585ed078e8d5
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-02-15 14:08:26 +0300

    Adding Core

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        public T Get(Expression<Func<T, bool>> filter);
+    }
+}
