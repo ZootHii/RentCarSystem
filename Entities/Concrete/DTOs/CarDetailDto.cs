809f27d11412b345cb19530be56e4d1b5d8b89d6
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-02-15 14:04:43 +0300

    Refactoring Entities

--- Entities/Concrete/DTOs/CarDetailDto.cs
+++ Entities/Concrete/DTOs/CarDetailDto.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using Core.Entities;
 
-namespace Entities.Concrete
+namespace Entities.Concrete.DTOs
 {
-    public class Car : IEntity
+    public class CarDetailDto : IDto
     {
         public int Id { get; set; }
-        public int BrandId { get; set; }
-        public int ColorId { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
         public DateTime ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Description { get; set; }
-
+        
         public override string ToString()
         {
             return "ID = " + Id +
-                   " / BrandID = " + BrandId +
-                   " / ColorID = " + ColorId +
+                   " / BrandName = " + BrandName +
+                   " / ColorName = " + ColorName +
                    " / ModelYear = " + ModelYear.Year +
                    " / DailyPrice = " + DailyPrice +
                    " / Description = " + Description;
