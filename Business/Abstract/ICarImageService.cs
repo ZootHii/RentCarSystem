d28db1c453488b40aa3bbc333f379c577d939651
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-02-27 17:17:51 +0300

    Service and Manager added in Business for CarImage entity

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        public IResult Add(CarImage carImage);
+        public IResult Update(CarImage carImage);
+        public IResult Delete(CarImage carImage);
+        public IDataResult<Car> GetCarImageById(int carImageId);
+        public IDataResult<List<CarImage>> GetAllCarImages();
+        public IDataResult<List<CarImage>> GetCarImagesByCarId(int carId);
+    }
+}
---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        private readonly ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+        
+        public IResult Add(CarImage carImage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(CarImage carImage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<Car> GetCarImageById(int carImageId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAllCarImages()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetCarImagesByCarId(int carId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
