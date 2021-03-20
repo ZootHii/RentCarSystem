966d14bf4b8a8057eb1e74ea6c8ad2fc76b2fb2a
Ahmet <ahmet-yildirim_5858@outlook.com>
2021-03-20 17:40:02 +0300

    Swagger UI Authorization Option added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Core.DependencyResolvers;
 using Core.Extensions;
 using Microsoft.AspNetCore.Builder;
@@ -22,7 +23,43 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebAPI", Version = "v1"}); });
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "WebAPI", Version = "v1"
+                });
+                
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme." +
+                                  "\r\n\r\n Enter the example." +
+                                  "\r\n\r\nExample: \"Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
             services.AddDependencyResolvers(new CoreModule()); // Extension
         }
 
